5c207573cf8ea6940d0ad96fb063598e091d069c
mscteknoloji <tguler@mscteknoloji.com>
2024-09-23 11:53:20 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import './global.css';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test for nativewind</Text>
+    </View>
+  );
+};
+
+export default App;
