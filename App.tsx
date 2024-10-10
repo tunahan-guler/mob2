912eb992c17588aba11218e10d4ec151268bcf2a
mscteknoloji <tguler@mscteknoloji.com>
2024-10-10 13:55:26 +0300

    ch

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './global.css';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/Router/Router';
 import {Text} from 'react-native';
 
 const App = () => {
+  const [bgColor, setBgColor] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setBgColor('text-primary');
+    }, 1000);
+  }, []);
+
   return (
     <NavigationContainer>
-      <Text className="text-primary ">
+      <Text className={`${bgColor}`}>
         OUTSIDE NAVIGATION PRIMARY WITHOUT VARIABLE
       </Text>
       <Text className="text-variable-primary">
